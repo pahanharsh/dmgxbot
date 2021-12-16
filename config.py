05adb388319dda402f8cae9a7fb33fb9da642405
pahanharsh <82528202+pahanharsh@users.noreply.github.com>
2021-12-16 21:52:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 class Config:
-    BOT_TOKEN = "your bot " 
-    API_ID =  "your api id "
-    API_HASH = "you api hash"
-    
+    BOT_TOKEN = "5049958356:AAGc9hhLLus_vS-WcBu0jma4vcXbEk6FQvw"             
+    API_ID =  "3084289"                                                      
+    API_HASH = "8c833d6d8b4a762febd19e73c1bb7c8c
